31301c802333cb070e3d3d1601188a35b05c4789
arge-7 <68198440+arge-7@users.noreply.github.com>
2021-01-26 12:55:30 -0600

    Update opencv.py

--- dlclivegui/camera/opencv.py
+++ dlclivegui/camera/opencv.py
@@ -14,6 +14,34 @@ import platform
 
 from dlclivegui.camera import Camera, CameraError
 
+# copy pasting Jetson gstreamer code
+def gstreamer_pipeline(
+    capture_width=1280,
+    capture_height=720,
+    display_width=1280,
+    display_height=720,
+    framerate=60,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, "
+        "format=(string)NV12, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+# end copy pasting jetson gstreamer code
 
 class OpenCVCam(Camera):
     @staticmethod
